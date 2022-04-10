37bb0709bcb6bf287dce68d13ccdbf45d3f4db79
Sergey Bachurin <ba4urin.sergey2016@yandex.ru>
2022-04-10 16:45:22 +0500

    Зарегистрировал новый класс, чтобы появились новые поля в модели FlatPages

--- first_flatpages/first_flatpages/settings.py
+++ first_flatpages/first_flatpages/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django.contrib.sites',
-    'django.contrib.flatpages'
+    'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
